dfb51d802df74c2c5cba582dbd079e9436550468
Shubham Asthana <shubham.asthana3092@gmail.com>
2025-05-02 21:50:36 +0530

    Initial Commit

---
+++ practice/src/main/java/com/programming/dsalgo/App.java
@@ -0,0 +1,13 @@
+package com.programming.dsalgo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
