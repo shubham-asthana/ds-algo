4ec406c29b43340c244f361f5213f0148563f576
Shubham Asthana <shubham.asthana3092@gmail.com>
2025-06-17 00:32:31 +0530

    Removed practice files

--- practice/src/main/java/com/programming/service/FruitService.java
+++
@@ -1,9 +0,0 @@
-package com.programming.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface FruitService {
-
-    public Map<String, Integer> countFruits(List<String> fruits);
-}
