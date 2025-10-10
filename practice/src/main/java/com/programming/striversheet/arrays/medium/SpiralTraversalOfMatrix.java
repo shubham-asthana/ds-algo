31fca901e6229a4c238c1c64dcd8aae5cfc359ef
Shubham Asthana <shubham.asthana3092@gmail.com>
2025-10-11 00:42:00 +0530

    Spiral Matrix

--- practice/src/main/java/com/programming/striversheet/arrays/medium/SpiralTraversalOfMatrix.java
+++ practice/src/main/java/com/programming/striversheet/arrays/medium/SpiralTraversalOfMatrix.java
@@ -1,5 +1,6 @@
 package com.programming.striversheet.arrays.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralTraversalOfMatrix {
@@ -14,7 +15,35 @@ public class SpiralTraversalOfMatrix {
             System.out.print(num + " ");
     }
 
+    // right -> bottom -> left -> top
+    // initially, left = 0, right = n - 1, top = 0, bottom = n - 1
+    // T.C = O(N * M). S.C = O(N * M) for ans
     private static List<Integer> spiralOrder(int[][] matrix) {
-
+        List<Integer> result = new ArrayList<>();
+        int left = 0, right = matrix[0].length - 1, top = 0, bottom = matrix.length - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) { // iterate from right to left only if top has not exceeded bottom => row is
+                                 // there to print. If only one row, this loop wil not run.
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) { // If there are columns left then print, else skip
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
     }
 }
